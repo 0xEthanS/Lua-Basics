dd37645847b7e869f4a2fd48dfcc1c7f0ee5528d
Ethan <ethans228@yahoo.com>
2024-10-15 22:01:10 -0400

    Only commit

---
+++ app.lua
@@ -0,0 +1,16 @@
+-- This is a single line comment in lua
+
+
+--[[
+This is a multi-line comment in lua
+]]
+
+
+-- Print Statement
+print("Hello World")
+
+
+
+
+-- https://www.lua.org/pil/contents.html
+-- Lua is stupid easy, damn
